a07e3184f61b27bc57eda8be7d22c4572f8ed382
guidomarchi <gm191201@gmail.com>
2024-08-27 17:27:28 +0000

    a

--- client/src/client.c
+++ client/src/client.c
@@ -18,17 +18,24 @@ int main(void)
 
 	// Usando el logger creado previamente
 	// Escribi: "Hola! Soy un log"
-
+	log_info(logger, "Hola! Soy un log");
 
 	/* ---------------- ARCHIVOS DE CONFIGURACION ---------------- */
 
 	config = iniciar_config();
-
+	if(config == NULL){
+		abort();
+	}
 	// Usando el config creado previamente, leemos los valores del config y los 
 	// dejamos en las variables 'ip', 'puerto' y 'valor'
+	ip = config_get_string_value(config, "IP");
+	puerto = config_get_string_value(config, "PUERTO");
+	valor = config_get_string_value(config, "CLAVE");
 
 	// Loggeamos el valor de config
-
+	log_info(logger,ip);
+	log_info(logger,puerto);
+	log_info(logger,valor);
 
 	/* ---------------- LEER DE CONSOLA ---------------- */
 
@@ -42,6 +49,7 @@ int main(void)
 	conexion = crear_conexion(ip, puerto);
 
 	// Enviamos al servidor el valor de CLAVE como mensaje
+	enviar_mensaje(valor,conexion);
 
 	// Armamos y enviamos el paquete
 	paquete(conexion);
@@ -55,14 +63,15 @@ int main(void)
 t_log* iniciar_logger(void)
 {
 	t_log* nuevo_logger;
-
+	nuevo_logger = log_create("tp0.log", "Mi log", true, LOG_LEVEL_INFO);
+	log_info(nuevo_logger,"Soy un Log");
 	return nuevo_logger;
 }
 
 t_config* iniciar_config(void)
 {
 	t_config* nuevo_config;
-
+	nuevo_config =config_create("/home/utnso/tp0/client/cliente.config");
 	return nuevo_config;
 }
 
@@ -74,7 +83,12 @@ void leer_consola(t_log* logger)
 	leido = readline("> ");
 
 	// El resto, las vamos leyendo y logueando hasta recibir un string vacío
-
+	while(strcmp(leido,"")!=0){
+		leido = readline("> ");
+		log_info(logger,leido);
+		free(leido);
+	}
+	free(leido);
 
 	// ¡No te olvides de liberar las lineas antes de regresar!
 
@@ -87,7 +101,15 @@ void paquete(int conexion)
 	t_paquete* paquete;
 
 	// Leemos y esta vez agregamos las lineas al paquete
-
+	paquete = crear_paquete();
+	leido = readline("> ");
+	while(strcmp(leido,"")!=0){
+		leido = readline("> ");
+		agregar_a_paquete(paquete,leido,sizeof(leido));
+		free(leido);
+	}
+	enviar_paquete(paquete, conexion);
+	free(leido);
 
 	// ¡No te olvides de liberar las líneas y el paquete antes de regresar!
 	
@@ -97,4 +119,7 @@ void terminar_programa(int conexion, t_log* logger, t_config* config)
 {
 	/* Y por ultimo, hay que liberar lo que utilizamos (conexion, log y config) 
 	  con las funciones de las commons y del TP mencionadas en el enunciado */
+	log_destroy(logger);
+	config_destroy(config);
+	liberar_conexion(conexion);
 }
--- client/src/utils.c
+++ client/src/utils.c
@@ -26,15 +26,27 @@ int crear_conexion(char *ip, char* puerto)
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
 
-	getaddrinfo(ip, puerto, &hints, &server_info);
+	int status = getaddrinfo(ip, puerto, &hints, &server_info);
 
+	if (status != 0) {
+    	printf("Error en getaddrinfo: %s\n", gai_strerror(status));
+    	return -1;
+	}
 	// Ahora vamos a crear el socket.
-	int socket_cliente = 0;
+	int socket_cliente = socket(server_info->ai_family,server_info->ai_socktype,server_info->ai_protocol);
+	if(socket_cliente == -1){
+		printf("Error al crear el socket");
+		return -1;
+	}
 
 	// Ahora que tenemos el socket, vamos a conectarlo
-
-
-	freeaddrinfo(server_info);
+	int resultado = connect(socket_cliente, server_info->ai_addr, server_info->ai_addrlen);
+	if (resultado == -1) {
+        printf("Error al conectar al servidor");
+        close(socket_cliente);
+        return -1;
+	}
+	//freeaddrinfo(server_info);
 
 	return socket_cliente;
 }
